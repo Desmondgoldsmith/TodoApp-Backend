1a6721f6301b20226236f757471701198662f281
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-09-29 23:23:48 +0000

    chore/ installed psycopg postgres driver

--- app/main.py
+++ app/main.py
@@ -1,4 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response,status, HTTPException, Depends
+import psycopg
+from psycopg.rows import dict_row
+from sqlalchemy.orm import Session
+from .database import engine, get_db
+from . import models, schema
+from typing import List
+import time
+
+models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
@@ -8,3 +17,9 @@ app = FastAPI()
 @app.get('/hello')
 def HelloWorld():
     return {"data": "Hello World !"}
+
+# endpoint to get all todos
+@app.get('/todos')
+def getTodos():
+    return {"status": "successful"
+           }
